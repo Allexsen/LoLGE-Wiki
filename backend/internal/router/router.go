4eac7b4095a0a294a23f427c532973b391d34db6
Alex <asvanidze12@gmail.com>
2023-12-18 23:03:35 +0100

    Routing Structure Template
    Created and added some placeholder structures for routing, handler functions and required packages.

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,1 +1,39 @@
 package router
+
+import (
+	"log"
+	"os"
+
+	"github.com/Allexsen/LoLGE-Wiki/backend/internal/handler"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+var (
+	r *gin.Engine
+
+	DIR_FRONT string
+)
+
+func GetEngine() *gin.Engine {
+	return r
+}
+
+// Tester route
+func InitRouter() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("[Error]: Couldn't load .env file")
+	}
+
+	DIR_FRONT = os.Getenv("DIR_FRONT")
+	r.GET("/", func(c *gin.Context) {
+		c.File(DIR_FRONT + "/html/home.html")
+	})
+
+	r.Run(":5000")
+}
+
+func setupChampionRoutes() {
+	r.GET("/champions/", handler.ChampionsGET)
+}
